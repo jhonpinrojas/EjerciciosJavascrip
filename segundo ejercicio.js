ac860b4f2519d2efa3e1680a5edff9e18371bd15
jhonpinrojas <jhonpinrojas@gmail.com>
2022-09-21 16:11:24 -0500

    2 proyecto

---
+++ segundo ejercicio.js
@@ -0,0 +1,13 @@
+let nombre= "Jhon edison pinedo";
+let edad=33;
+let desarrollador=true;
+let date= new Date("october 22 1988");
+var libro={
+    titulo:"la fortaleza Diguital",
+    autor:"Dan brown",
+    fecha:1998,
+    url:"https://es.wikipedia.org/wiki/La_fortaleza_digital"
+
+}
+var lista=[nombre,edad,desarrollador,date,libro]
+console.log(lista);  
